f9468e751166b402d37e306252699d0366d1cbfe
Salmi Azz Eddine <salmi.azzeddine100@gmail.com>
2023-06-05 21:03:27 +0200

    adding pagination

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -18,9 +18,12 @@ class ProductController extends AbstractController
     /**
      * @Route("/api/products", name="listProducts", methods={"GET"})
      */
-    public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer, Request $request): JsonResponse
     {
-        $productList = $productRepository->findAll();
+        $page = $request->get('page', 1);
+        $limit = $request->get('limit', 3);
+
+        $productList = $productRepository->findAllWithPagination($page, $limit);
 
         $jsonProductList = $serializer->serialize($productList, 'json');
         return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
