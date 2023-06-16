2ba3c97690a685793749e2997e1409b142dd7a39
Salmi Azz Eddine <salmi.azzeddine100@gmail.com>
2023-06-17 00:03:09 +0200

    adding groups into Customer Entity

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -6,6 +6,7 @@ use App\Repository\CustomerRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=CustomerRepository::class)
@@ -16,18 +17,20 @@ class Customer
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"getCustomers", "getUsers"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"getCustomers", "getUsers"})
      */
-    private $name;
+    private ?string $name;
 
     /**
-     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="customers")
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="customers", cascade={"all"})
      */
-    private $User;
+    private ?User $User;
 
     public function getId(): ?int
     {
